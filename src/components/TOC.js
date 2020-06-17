43554de99d78fe0a39aca6bb12569358a6d3c6ac
rain.i <rain.i@kakaoenterprise.com>
2020-06-17 09:11:24 +0900

    first commit

---
+++ src/components/TOC.js
@@ -0,0 +1,33 @@
+
+import React, { Component } from 'react';
+
+class TOC extends Component {
+    render() {
+      console.log("TOC render...")
+      var lists = [];
+      var data = this.props.data;
+      for( var i=0; i<data.length; i++) {
+        lists.push(
+            <li key={data[i].id}>
+                <a 
+                    href={"/content/" + data[i].id}
+                    data-id={data[i].id}
+                    onClick={function(e){
+                        // debugger;
+                        e.preventDefault();
+                        this.props.onChangePage(e.target.dataset.id);
+                    }.bind(this)}
+                    >{data[i].title}</a>
+            </li>)
+      }
+      return(
+        <nav>
+          <ul>
+              {lists}
+          </ul>
+        </nav>
+      );
+    }
+  }
+  
+export default TOC;
